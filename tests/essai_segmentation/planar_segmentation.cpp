f10e229c91f05a9625888664f2cce0c610638a69
AurelienMassiot <aurelien.massiot@live.com>
2015-03-08 17:00:37 +0100

    Tests : Ajout méthodes de segmentation

---
+++ tests/essai_segmentation/planar_segmentation.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/visualization/pcl_visualizer.h>
+
+
+boost::shared_ptr<pcl::visualization::PCLVisualizer> simpleVis (pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud)
+{
+  // --------------------------------------------
+  // -----Open 3D viewer and add point cloud-----
+  // --------------------------------------------
+  boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+  viewer->setBackgroundColor (0, 0, 0);
+  
+  // utiliser plusieurs fois addPointCloud si on veut faire apparaitre plusieurs nuages de points
+  viewer->addPointCloud<pcl::PointXYZ> (cloud, "sample cloud");
+
+  // on peut changer la taille des points
+  viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "sample cloud");
+
+  // mettre à 0 si on ne veut pas que les axes apparaissent
+  viewer->addCoordinateSystem (1.0);
+
+  viewer->initCameraParameters ();
+  return (viewer);
+}
+
+
+int
+ main (int argc, char** argv)
+{
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+  // Fill in the cloud data
+  cloud->width  = 15;
+  cloud->height = 1;
+  cloud->points.resize (cloud->width * cloud->height);
+
+  // Generate the data
+  for (size_t i = 0; i < cloud->points.size (); ++i)
+  {
+    cloud->points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
+    cloud->points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
+    cloud->points[i].z = 1.0;
+  }
+
+  // Set a few outliers (des points qui ne seront volontairement pas dans le plan qu'on trouvera)
+  cloud->points[0].z = 2.0;
+  cloud->points[3].z = -2.0;
+  cloud->points[6].z = 4.0;
+
+  std::cerr << "Point cloud data: " << cloud->points.size () << " points" << std::endl;
+  for (size_t i = 0; i < cloud->points.size (); ++i)
+    std::cerr << "    " << cloud->points[i].x << " "
+                        << cloud->points[i].y << " "
+                        << cloud->points[i].z << std::endl;
+
+  pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+  pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+  // Create the segmentation object
+  pcl::SACSegmentation<pcl::PointXYZ> seg;
+  // Optional
+  seg.setOptimizeCoefficients (true);
+  // Mandatory
+  seg.setModelType (pcl::SACMODEL_PLANE);
+  seg.setMethodType (pcl::SAC_RANSAC);
+  seg.setDistanceThreshold (0.01);
+
+  seg.setInputCloud (cloud);
+  seg.segment (*inliers, *coefficients);
+
+  if (inliers->indices.size () == 0)
+  {
+    PCL_ERROR ("Could not estimate a planar model for the given dataset.");
+    return (-1);
+  }
+
+  // paramètres du plan
+  std::cerr << "Model coefficients: " << coefficients->values[0] << " " 
+                                      << coefficients->values[1] << " "
+                                      << coefficients->values[2] << " " 
+                                      << coefficients->values[3] << std::endl;
+
+  // points qui font partie du plan déterminé par segmentation plane
+  std::cerr << "Model inliers: " << inliers->indices.size () << std::endl;
+  for (size_t i = 0; i < inliers->indices.size (); ++i)
+    std::cerr << inliers->indices[i] << "    " << cloud->points[inliers->indices[i]].x << " "
+                                               << cloud->points[inliers->indices[i]].y << " "
+                                               << cloud->points[inliers->indices[i]].z << std::endl;
+
+  // voir tous les points graphiquement, mais sans couleur (on ne voit donc pas la segmentation...)
+
+  boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;
+  viewer = simpleVis(cloud);
+  while (!viewer->wasStopped ())
+  {
+    viewer->spinOnce (100);
+    boost::this_thread::sleep (boost::posix_time::microseconds (100000));
+  }
+
+  return (0);
+
+}
