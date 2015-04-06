48e9bce9ab9519f0d9dfd18a1be2655ff18d1cfc
Antoine Augusti <antoine@augusti.fr>
2015-04-06 15:46:45 +0200

    Ajout de commentaires sur le test avec kmeans

--- matlab/testKmeans.m
+++ matlab/testKmeans.m
@@ -5,8 +5,11 @@ data = load('../dataset/dish_area_dataset/attributes.csv');
 
 clusters = kmeans(data, 2);
 
+% Gets the columns from the text file
 [nb1, nb2, labels] = textread('dish_area_labels.txt', '%f %f %s')
 
+% Replaces the label background with 1 and the label car with 2
 labelsNum = strcmp(labels, 'background') + 2 * strcmp(labels, 'car');
 
+% Calculate the error rate
 err = sum(labelsNum ~= clusters) / length(clusters) * 100;
