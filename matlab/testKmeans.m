627f65e7b721bfa3056f9f8cee292d662272ee9b
Manon Ansart <manon.ansart@gmail.com>
2015-05-21 19:59:20 +0200

    Amélioration k moyennes et résultats

--- matlab/testKmeans.m
+++ matlab/testKmeans.m
@@ -2,14 +2,31 @@ close all
 clear all
 
 data = load('../dataset/dish_area_dataset/attributes.csv');
+Y = load('../dataset/dish_area_dataset/labels.csv');
 
 clusters = kmeans(data, 2);
 
-% Gets the columns from the text file
-[nb1, nb2, labels] = textread('dish_area_labels.txt', '%f %f %s')
 
-% Replaces the label background with 1 and the label car with 2
-labelsNum = strcmp(labels, 'background') + 2 * strcmp(labels, 'car');
+nbClasses = 2;
+nvLabels = [];
+for i = 1:nbClasses
+	compteur = [];
+	for j = 1:nbClasses
+		compteur = [compteur sum((Y == i) .* (clusters == j))];
+	end	
+	[tmp nvLabel] = max(compteur);
+	nvLabels = [nvLabels nvLabel];
+end
+
+for i = 1:nbClasses
+	clusters(find(clusters == i)) = nvLabels(i);
+end
 
 % Calculate the error rate
-err = sum(labelsNum ~= clusters) / length(clusters) * 100;
+erreur = sum(Y ~= clusters) / length(clusters) * 100
+
+% Matrice de confusion
+disp(strcat('Prediction: background | Truth : background : ', num2str(sum((clusters == 1) .* (Y == 1)))))
+disp(strcat('Prediction: background | Truth : car : ', num2str(sum((clusters == 1) .* (Y == 2)))))
+disp(strcat('Prediction: car | Truth : car : ', num2str(sum((clusters == 2) .* (Y == 2)))))
+disp(strcat('Prediction: car | Truth : background : ', num2str(sum((clusters == 2) .* (Y == 1)))))
